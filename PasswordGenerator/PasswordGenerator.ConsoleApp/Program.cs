d7607db823beeff668602f333c340a26c64caede
Luiz Diniz <luiz.gustavon@hotmail.com>
2024-06-09 17:26:18 -0300

    - Console validations refactor

--- PasswordGenerator/PasswordGenerator.ConsoleApp/Program.cs
+++ PasswordGenerator/PasswordGenerator.ConsoleApp/Program.cs
@@ -12,43 +12,38 @@ namespace PasswordGenerator.ConsoleApp
         {
             var passwordOptions = new PasswordOptions();
             var passwordGenerator = new Core.PasswordGenerator(new StringGenerator());
-            var validInput = false;
 
             Console.WriteLine("----- PasswordGenerator -----\n\n");
 
             try
             {
-                do
-                {
-                    Console.WriteLine("-- Select which elements you want on the password using 'y or n' for each of them.\n");
+                Console.WriteLine("- Include Numbers (y or n): ");
+                LoopBooleanInsertion(out var numberValue);
+                passwordOptions.Numbers = numberValue;
 
-                    Console.WriteLine("- Numbers: ");
-                    passwordOptions.Numbers = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+                Console.WriteLine("\n- Include Special Characters (y or n): ");
+                LoopBooleanInsertion(out var specialCharacters);
+                passwordOptions.SpecialCharacters = specialCharacters;
 
-                    Console.WriteLine("\n- Special Characters: ");
-                    passwordOptions.SpecialCharacters = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+                Console.WriteLine("\n- Include UpperCase Characters (y or n): ");
+                LoopBooleanInsertion(out var upperCharacters);
+                passwordOptions.UpperCaseCharacters = upperCharacters;
 
-                    Console.WriteLine("\n- Uppercase letters: ");
-                    passwordOptions.UpperCaseCharacters = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+                Console.WriteLine("\n- Include LowerCase Characters (y or n): ");
+                LoopBooleanInsertion(out var lowerCharacters);
+                passwordOptions.LowerCaseCharacters = lowerCharacters;
 
-                    Console.WriteLine("\n- Lowecase letters: ");
-                    passwordOptions.LowerCaseCharacters = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+                Console.WriteLine("\n- Passwords Size (Max 100): ");
+                LoopIntegerInsertion(out var sizeValue);
+                passwordOptions.Size = sizeValue;
 
-                    Console.WriteLine("\n- Password size (Max 100): ");
-                    var size = 0;
-                    var sizeSuccess = Int32.TryParse(Console.ReadLine(), out size);
-                    passwordOptions.Size = size;
+                Console.WriteLine("\n- Quantity of passwords to be generated (Max 100): ");
+                LoopIntegerInsertion(out var quantityValue);
+                passwordOptions.Quantity = quantityValue;
 
-                    Console.WriteLine("\n- Quantity of passwords to be generated (Max 100): ");
-                    var quantity = 0;
-                    var quantitySuccess = Int32.TryParse(Console.ReadLine(), out quantity);
-                    passwordOptions.Quantity = quantity;
+                Console.Write($"Generating passwords... {Environment.NewLine}{Environment.NewLine}");
 
-                    Console.Write($"Generating passwords... {Environment.NewLine}{Environment.NewLine}");
-                    Console.WriteLine(BuildResult(passwordGenerator.GeneratePasswords(passwordOptions)));
-                    break;
-
-                } while (!validInput);
+                Console.WriteLine(BuildResult(passwordGenerator.GeneratePasswords(passwordOptions)));
             }
             catch (Exception ex)
             {
@@ -58,7 +53,58 @@ namespace PasswordGenerator.ConsoleApp
             Console.ReadKey();
         }
 
-        static string BuildResult(IEnumerable<string> passwords)
+        public static void LoopBooleanInsertion(out bool value)
+        {
+            var validInput = false;
+            value = false;
+
+            do
+            {
+                var insertedValue = Console.ReadLine().Trim();
+
+                if (String.Equals(insertedValue, "y", StringComparison.OrdinalIgnoreCase))
+                {
+                    validInput = true;
+                    value = true;
+                }
+                else if(String.Equals(insertedValue, "n", StringComparison.OrdinalIgnoreCase))
+                {
+                    validInput = true;
+                    value = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid value inserted. Try again: {Environment.NewLine} ");
+                }
+
+
+            } while (!validInput);
+        }
+
+        public static void LoopIntegerInsertion(out int value)
+        {
+            var validInput = false;
+            value = 0;
+
+            do
+            {
+                Int32.TryParse(Console.ReadLine(), out var quantity);
+
+                if(quantity > 0 && quantity <= 100)
+                {
+                    validInput = true;
+                    value = quantity;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid value inserted. Try again: {Environment.NewLine} ");
+                }
+
+
+            } while (!validInput);
+        }
+
+        public static string BuildResult(IEnumerable<string> passwords)
         {
             var builder = new StringBuilder();
 
